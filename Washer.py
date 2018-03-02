51b1ac74abe8081d920397b045ef6d69c1fbbc2f
Graham Parker <frymyox@gmail.com>
2018-03-03 11:30:58 +1300

    Add files via upload

--- Washer.py
+++ Washer.py
@@ -2,19 +2,31 @@ import re
 
 class Washer(object):
     #Graham
-    def keep_only_letters_numbers(self, data):
-        characters_to_keep = re.compile(r"[^a-zA-Z0-9]")
-        washed_data = characters_to_keep.sub("", data)
+    # For example calling:
+    #   keep_only_these_in_string("jun99", "a-z")
+    # Returns:
+    #   "jun"
+    #
+    # Because it only keeps letters a-z
+    # a-zA-Z0-9 would keep all letters and numbers
+    def keep_only_these_in_string(self, data, regex_to_keep):
+        regex_target = re.compile(r"[^" + regex_to_keep + "]")
 
-        return washed_data
+        data = regex_target.sub("", data)
+        return data
 
     # Graham
+    # Output will have one capital letter then all lowercase
+    # e.g. tEsT would become Test
     def set_case(self, data):
         washed_data = data.title()
 
         return washed_data
 
     # Graham
+    # For example calling:
+    #   replace_x_with_y("-", "/")
+    # Would change 10-01-1998 to 10/01/1998
     def replace_x_with_y(self, x, y):
         target = x
         replacement = y
