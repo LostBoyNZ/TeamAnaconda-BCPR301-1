cb57ad5a550b3772bf68dd95e487db6395caa411
Graham Parker <frymyox@gmail.com>
2018-03-06 22:20:46 +1300

    Add files via upload
    Our command line. Currently the classes and methods you can call are all in this file.
    
    It can be tested by entering "Check empid" which will return that the ID is valid. The next step is to add the ability to have a variable sent with the command, then the ability to call classes from other files.

--- cmd.py
+++ cmd.py
@@ -1,32 +1,47 @@
+import sys
 import validate_empid
 
-# getattr
 
-# class Cmd():
-#
-#     def command(self):
-#         userCommand = input("> ")
-#
-#         try:
-#             #className = Hi
-#             i = userCommand
-#             output = i.has_this_many_numbers(i, 3, "302")
-#             print(output)
-#             #newObject = className
-#             #className = userCommand
-#         except NameError:
-#             print("Command not found")
-#
-# class Hi():
-#     def run(self):
-#         print("hi")
-#
-#     print("hello")
-#
-# i = Cmd()
-# i.command()
+class Check():
+    def __init__(self):
+        print("!!! Class Check Reached !!!")
 
-# This checks if an ID is valid
-i = validate_empid.ValidateEmpid
-output = i.is_valid(i, "a#2@$&*(@&$01")
-print(output)
+    @staticmethod
+    def empid():
+        i = validate_empid.ValidateEmpid
+        output = i.is_valid(i, "a#2@$&*(@&$01")
+        if output:
+            print("Valid ID")
+        else:
+            print("Invalid ID")
+
+class CommandLine():
+    user_command = input("> ")
+
+    split_command = user_command.split(" ")
+
+    class_to_call = split_command[0].title()
+    method_to_call = ""
+    string_to_send = ""
+
+    if len(split_command) > 1:
+        method_to_call = split_command[1].lower()
+        if len(split_command) > 2:
+            string_to_send = split_command[2]
+
+    if class_to_call:
+        try:
+            class_name = getattr(sys.modules[__name__], class_to_call)
+            class_name()
+        except AttributeError:
+            print("The command '{}' is not valid. Please enter Help to see a list of valid commands.".format(method_to_call))
+
+    if method_to_call:
+        try:
+            class_name = getattr(sys.modules[__name__], class_to_call)
+            method_name = getattr(class_name, method_to_call)
+            method_name()
+        except AttributeError:
+            print("The switch '{}' is not valid.".format(method_to_call))
+
+i = CommandLine()
