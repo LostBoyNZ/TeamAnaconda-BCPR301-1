36f3ac325d35eaa1da2d9992765da2b27604694d
Graham Parker <frymyox@gmail.com>
2018-03-10 16:22:38 +1300

    Add files via upload

---
+++ validators/validate_date.py
@@ -0,0 +1,148 @@
+# Graham
+
+import datetime
+import sys
+
+try:
+    #from validators.validator import Validator, is_correct_pattern, is_within_length, has_this_many_numbers, has_this_many_letters
+    from validators.validator import Validator
+except NameError and ModuleNotFoundError and ImportError:
+     print("Fatal Error - validator.py not found.")
+     sys.exit()
+
+try:
+    #from washer import Washer, replace_x_with_y, valid_date
+    from washers.washer import Washer
+except NameError and ModuleNotFoundError and ImportError:
+    print("Fatal Error - washer.py not found.")
+    sys.exit()
+
+
+class ValidateDate():
+
+    valid_date = ""
+
+    def split_string(self, delimiter, data):
+        return data.split(delimiter)
+
+    def add_zeros(self, data):
+        split_date = self.split_string("/", data)
+        day = split_date[0]
+        month = split_date[1]
+        year = split_date[2]
+
+        if day.isdigit():
+            day = day.zfill(2)
+        if month.isdigit():
+            month = month.zfill(2)
+
+        return day + "/" + month + "/" + year
+
+    def is_real_date(self, data, date_format):
+        result = False
+
+        try:
+            datetime.datetime.strptime(data, date_format)
+            result = True
+        except ValueError:
+            result = False
+
+        return result
+
+    def determine_month_format(self, data):
+        # default to number format like 09 for September
+        result = "%m"
+
+        date_to_check = Washer.replace_x_with_y("\W+", "/", data)
+        split_date = self.split_string("/", date_to_check)
+        for value in split_date:
+            if value.isalpha():
+                if len(value) > 3:
+                    result = "%B"
+                else:
+                    result = "%b"
+
+        return result
+
+    def month_string_to_number(self, data):
+
+        date_to_check = Washer.replace_x_with_y("\W+", "/", data)
+        split_date = self.split_string("/", date_to_check)
+        for value in split_date:
+            if value.isalpha():
+                print("Is alpha")
+                if len(value) > 3:
+                    # result = "%B"
+                    split_date[value] = datetime.strptime(value, '%B').month
+                else:
+                    # result = "%b"
+                    split_date[value] = datetime.strptime(value, '%b').month
+
+        return split_date[0] + "/" + split_date[1] + "/" + split_date[2]
+
+    def determine_date_format(self, data):
+        day_format = "%d"
+        month_format = self.determine_month_format(data)
+        #month_number = self.determine_month_format(data)
+        year_format = "%Y"
+        format = day_format + " " + month_format + " " + year_format
+        #format = "%d %m %Y"
+
+        return format
+
+    def wash_data(self, data_to_wash):
+        # replace any non-word character with a forward slash
+        data_to_wash =  Washer.replace_x_with_y("\W+", "/", data_to_wash)
+        # remove st, nd and rd from date, e.g. 21st, 22nd, 23rd
+        data_to_wash =  Washer.replace_x_with_y("st|nd|rd", "", data_to_wash)
+
+        data_to_wash = self.add_zeros(data_to_wash)
+
+        valid_date = data_to_wash
+
+        return data_to_wash
+
+    def is_valid(self, data_to_validate):
+        result = False
+        #date_format = '%d %m %Y'
+
+        data_to_validate = self.wash_data(data_to_validate)
+
+        # remove all spaces
+        data_to_validate = data_to_validate.strip()
+
+        #self.data = self.month_string_to_number(self.data)
+        date_format = self.determine_date_format(data_to_validate)
+        date_to_check = Washer.replace_x_with_y('\W+', " ", data_to_validate)
+        result = self.is_real_date(date_to_check, date_format)
+
+        print(data_to_validate + " is valid: " + str(result))
+
+        return result
+
+#
+# Add some asserts like...
+#
+# if not condition:
+#    raise AssertionError()
+#
+#
+
+i = ValidateDate()
+
+result = i.is_valid("01-02-2020")
+print(i.valid_date)
+result = i.is_valid("32-02-2020")
+print(i.valid_date)
+result = i.is_valid("18-12-2018")
+print(i.valid_date)
+result = i.is_valid("03 February 2018")
+print(i.valid_date)
+result = i.is_valid("3rd Jan 2018")
+print(i.valid_date)
+result = i.is_valid("1st Jan 2018")
+print(i.valid_date)
+result = i.is_valid("2nd Jan 2018")
+print(i.valid_date)
+result = i.is_valid("1/1/1998")
+print(i.valid_date)
---
+++ validators/validate_empid.py
@@ -0,0 +1,46 @@
+# Graham
+
+import sys
+
+try:
+    #from validator import Validator, is_correct_pattern, is_within_length, has_this_many_numbers, has_this_many_letters
+    from validators.validator import Validator
+except NameError and ModuleNotFoundError and ImportError:
+    print("Fatal Error - validator.py not found.")
+    sys.exit()
+
+try:
+    #from washer import Washer, keep_only_these_in_string, set_case
+    from washers.washer import Washer
+except NameError and ModuleNotFoundError and ImportError:
+    print("Fatal Error - washer.py not found.")
+    sys.exit()
+
+
+class ValidateEmpid():
+
+    @staticmethod
+    def is_valid(data_to_validate):
+        correct_length = False
+        correct_number_of_characters = False
+        correct_characters = False
+        correct_format = False
+
+        # Wash the data before verifying it
+        washed_data = Washer.keep_only_these_in_string("a-zA-Z0-9", data_to_validate)
+        washed_data = Washer.set_case(washed_data)
+
+        # \D where a non-number should be, and \d where a number should be
+        if Validator.is_correct_pattern("\D\d\d\d", data_to_validate) == True:
+            correct_format = True
+
+        if Validator.is_within_length(4, 4, data_to_validate) == True:
+            correct_length = True
+
+        if Validator.has_this_many_numbers(3, data_to_validate) == True and Validator.has_this_many_letters(1, data_to_validate) == True:
+            correct_number_of_characters = True
+
+        if data_to_validate.isalnum() == True:
+            correct_characters = True
+
+        return correct_length and correct_number_of_characters and correct_characters and correct_format
