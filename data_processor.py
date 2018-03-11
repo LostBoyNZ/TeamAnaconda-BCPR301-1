e9d21d30a674423a9fc8359971a72087778dfe6d
Granity <30912719+Granity@users.noreply.github.com>
2018-03-11 19:05:40 +1300

    Add files via upload

---
+++ data_processor.py
@@ -0,0 +1,13 @@
+class DataProcessor(object):
+
+    def display_row(self, *row):
+        # x = {}
+        # x.update(row[0])
+        # print(val)
+        # print(x.keys())
+        #
+        # print(x.values())
+        # print(x['gender'])
+        # print('new row')
+        print(row[0])
+
