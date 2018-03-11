8ada98f9c8bec5ae48c7db796c606ddf96559b11
Granity <30912719+Granity@users.noreply.github.com>
2018-03-11 19:06:11 +1300

    Add files via upload

--- validators/validate_gender.py
+++ validators/validate_gender.py
@@ -1,18 +1,15 @@
 # Claye
-from anaconda.validators.validator import Validator as Va
-from anaconda.washers.washer import Washer as Wa
+from validators.validator import Validator as Va
+from washers.washer import Washer as Wa
 
 
 class ValidateGender(object):
 
-    def __init__(self, gender):
-        self.gender = gender
-
-    def is_valid(self):
+    def is_valid(self, to_check):
         result = False
         gender_list_m = ['M', 'Boy', 'Male', 'Dude']
         gender_list_f = ['F', 'Girl', 'Female', 'Lady']
-        g = self.gender
+        g = to_check
         g = Wa.wash_all_but_string_characters(g)
         g = Wa.set_case(g)
         if Va.is_in_list(g, gender_list_m):
@@ -21,25 +18,4 @@ class ValidateGender(object):
         elif Va.is_in_list(g, gender_list_f):
             g = 'F'
             result = True
-        else:
-            g = "INVALID"
-        print("result: " + str(result))
-        print(g)
-        return g
-
-
-i = ValidateGender(' 4254 54  M ale  $#$@13%$5 14-')
-print(i.is_valid())
-
-i = ValidateGender(' 4254 54  L231Ady  $#$@13%$5 14-')
-print(i.is_valid())
-
-i = ValidateGender(' 4254 54  fale  $#$@13%$5 14-')
-print(i.is_valid())
-
-i = ValidateGender(' 4254 54  !fem ale  $#$@13%$5 14-')
-print(i.is_valid())
-
-i = ValidateGender(' 4254 54  b !oy  $#$@13%$5 14-')
-print(i.is_valid())
-
+        return g, result
