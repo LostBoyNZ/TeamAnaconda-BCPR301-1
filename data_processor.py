8169ec6da1a6a03bcf0686c0675e83dd001d6a94
Granity <30912719+Granity@users.noreply.github.com>
2018-03-12 21:21:43 +1300

    Add files via upload

--- data_processor.py
+++ data_processor.py
@@ -2,36 +2,59 @@ from validators.validate_gender import ValidateGender as vg
 from validators.validate_bmi import ValidateBmi as vb
 from validators.validate_date import ValidateDate as vd
 from validators.validate_empid import ValidateEmpid as ve
+from validators.validate_age import ValidateAge as va
+from validators.validate_salary import ValidateSalary as vs
+from validators.validate_sales import ValidateSales as vsa
+
 
 
 class DataProcessor(object):
 
     @staticmethod
     def send_to_validate(dict_root):
-        print(dict_root)
-        rows = 0
-        values = 0
+        valid_rows = 0
+        invalid_rows = 0
         for k, v in dict_root.items():
-            values += 1
+            valid = 0
+            length_of_values = len(v)
             if k != '':
-                print("EMPID")
                 result = ve.is_valid(k)
-                print(result)
+                if result:
+                    valid += 1
                 for kv in v.keys():
                     if kv == 'gender':
-                        print("GENDER")
                         result = vg.is_valid(vg, dict_root[k][kv])
-                        print(result)
+                        if result[1]:
+                            valid += 1
                     if kv == 'bmi':
-                        print("BMI")
                         result = vb.is_valid(vb, dict_root[k][kv])
-                        print(result)
+                        if result[1]:
+                            valid += 1
                     if kv == 'birthday':
-                        print("BIRTHDAY")
-                        print(dict_root[k][kv])
                         i = vd()
                         result = i.is_valid(dict_root[k][kv])
-                        print("result" + str(result))
-        print("number of values:")
-        print(values)
+                        if result[1]:
+                            valid += 1
+                    if kv == 'age':
+                        i = va()
+                        result = i.is_valid(dict_root[k][kv])
+                        if result[1]:
+                            valid += 1
+                    if kv == 'salary':
+                        i = vs()
+                        result = i.is_valid(dict_root[k][kv])
+                        if result[1]:
+                            valid += 1
+                    if kv == 'sales':
+                        i = vsa()
+                        result = i.is_valid(dict_root[k][kv])
+                        if result[1]:
+                            valid += 1
+                if valid == length_of_values:
+                    dict_root[k]['valid'] = '1'
+                    valid_rows += 1
+                else:
+                    invalid_rows += 1
+        print("{0} Rows Of Valid Data".format(valid_rows))
+        print("{0} Rows Of Invalid Data".format(invalid_rows))
         return dict_root
