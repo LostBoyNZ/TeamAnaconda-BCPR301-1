b7b54f97bbdede55c3540e1c025467dc5e709fa9
Granity <30912719+Granity@users.noreply.github.com>
2018-03-12 17:31:50 +1300

    Add files via upload

--- file_reader.py
+++ file_reader.py
@@ -1,9 +1,5 @@
 import errors
 from data_processor import DataProcessor
-from validators.validate_gender import ValidateGender as vg
-from validators.validate_bmi import ValidateBmi as vb
-from validators.validate_date import ValidateDate as vd
-from validators.validate_empid import ValidateEmpid as ve
 
 
 class FileReader(object):  # Claye
@@ -13,51 +9,6 @@ class FileReader(object):  # Claye
     def __init__(self):
         pass
 
-    def split_file(self, file_name): # Claye
-        file = open(file_name, "r")
-        # Repeat for each line in the text file
-        f = FileReader()
-        for line in file:
-            # Split the file into different fields using "," to split fields
-            fields = line.split(",")
-            f.dict_root.update({ fields[0]: {'gender': fields[1], 'age': fields[2], 'sales': fields[3], 'bmi': fields[4], 'salary': fields[5], 'birthday': fields[6].rstrip()}})
-        # Close the file to free up resources (good practice)
-        file.close()
-        self.send_to_validate()
-
-    def send_to_validate(self):
-        for k, v in self.dict_root.items():
-            if k != '':
-                print("EMPID")
-                result = ve.is_valid(k)
-                print(result)
-                for kv in v.keys():
-                    if kv == 'gender':
-                        print("GENDER")
-                        result = vg.is_valid(vg, self.dict_root[k][kv])
-                        print(result)
-                    if kv == 'bmi':
-                        print("BMI")
-                        result = vb.is_valid(vb, self.dict_root[k][kv])
-                        print(result)
-                    # if kv == 'birthday':
-                    #     print("BIRTHDAY")
-                    #     print(self.dict_root[k][kv])
-                    #     result = vd.is_valid(vd, self.dict_root[k][kv])
-                    #     print(result)
-
-                # print(self.dict_root[z][o])
-
-    # def send_to_validate(self):
-    #     for k, v in self.dict_root.items():
-    #         o = 'gender'
-    #         print(v.keys())
-    #         # print(x)
-    #         if k != '':
-    #             print(v['gender'])
-    #             print(self.dict_root[k][o])
-    #         # print(self.dict_root[z][o])
-
     def call_file(self):
         y = input("Please enter the filename: ")
         try:
@@ -65,17 +16,31 @@ class FileReader(object):  # Claye
         except FileNotFoundError:
             print(errors.ErrorHandler.get_error_message(201))
 
-    def write_file(self):
+    def split_file(self, file_name): # Claye
+        file = open(file_name, "r")
+        # Repeat for each line in the text file
+        f = FileReader()
+        del f.dict_root['']
+        for line in file:
+            # Split the file into different fields using "," to split fields
+            fields = line.split(",")
+            f.dict_root.update({ fields[0]: {'gender': fields[1], 'age': fields[2], 'sales': fields[3], 'bmi': fields[4], 'salary': fields[5], 'birthday': fields[6].rstrip()}})
+        # Close the file to free up resources (good practice)
+        file.close()
+        valid_dict = DataProcessor.send_to_validate(f.dict_root)
+        self.write_file(valid_dict)
+
+    def write_file(self, dict_valid):
         u = input("Are you sure you want to save data? Y/N > ")
         if u.upper() == "Y":
-            f = FileReader()
             z = open("data.txt", "a")
-            for key in f.dict_root:
-                z.write(str([key]) + '\n')
-                for value in f.dict_root[key]:
-                    h = str(f.dict_root[key][value])
+            for key in dict_valid:
+                z.write("\n")
+                z.write(key + ",")
+                for value in dict_valid[key]:
+                    h = str(dict_valid[key][value] + ",")
                     z.write(value + ' ' + h)
-                    z.write("\n")
+            z.write("\n")
             z.close()
             print("File saved")
         elif u.upper() == "N":
@@ -85,6 +50,5 @@ class FileReader(object):  # Claye
             self.write_file()
 
 
-i = FileReader()
-i.call_file()
-# i.write_file()
+# i = FileReader()
+# i.call_file()
