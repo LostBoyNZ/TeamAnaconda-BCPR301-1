6ddca640fcb7794a720c79490ec07d913cb8023b
rmw0251 <30913291+rmw0251@users.noreply.github.com>
2018-03-13 20:29:02 +1300

    Add files via upload

--- charts/calc_chart_data.py
+++ charts/calc_chart_data.py
@@ -51,65 +51,68 @@ class CalcData(object):
         return result
 
     def calculate(self, file_contents):
+        dict_data = {}
         for line in file_contents[1:]:
             fields = line.split(",")
-            for item in fields[1:-2]:
-                items = item.split(" ")
-                dict = {items[0]: items[1]}
-                for key, value in dict.items():
-                    if key == 'gender':
-                        if value == 'M':
-                            self.count_gender_m += 1
-                        if value == 'F':
-                            self.count_gender_f += 1
-                    elif key == 'age':
-                        self.age_list += [value]
-                    elif key == 'sales':
-                        if 0 <= int(value) >= 249:
-                            self.count_sales_group1 += 1
-                        elif 250 <= int(value) >= 499:
-                            self.count_sales_group2 += 1
-                        elif 500 <= int(value) >= 749:
-                            self.count_sales_group3 += 1
-                        elif 750 <= int(value) >= 999:
-                            self.count_sales_group4 += 1
-                    elif key == 'bmi':
-                        if value == 'Overweight':
-                            self.count_bmi_ov += 1
-                        elif value == 'Obesity':
-                            self.count_bmi_ob += 1
-                        elif value == 'Underweight':
-                            self.count_bmi_un += 1
-                        elif value == 'Normal':
-                            self.count_bmi_no += 1
-                    elif key == 'salary':
-                        self.salary_list += [value]
-                    elif key == 'birthday':
-                        month = value.split('/')[0]
-                        if month == '01':
-                            self.count_birth_jan += 1
-                        elif month == '02':
-                            self.count_birth_feb += 1
-                        elif month == '03':
-                            self.count_birth_mar += 1
-                        elif month == '04':
-                            self.count_birth_apr += 1
-                        elif month == '05':
-                            self.count_birth_may += 1
-                        elif month == '06':
-                            self.count_birth_jun += 1
-                        elif month == '07':
-                            self.count_birth_jul += 1
-                        elif month == '08':
-                            self.count_birth_aug += 1
-                        elif month == '09':
-                            self.count_birth_sep += 1
-                        elif month == '10':
-                            self.count_birth_oct += 1
-                        elif month == '11':
-                            self.count_birth_nov += 1
-                        elif month == '12':
-                            self.count_birth_dec += 1
+            if 'valid 1' in fields:
+                for item in fields[1:-1]:
+                    print(item)
+                    items = item.split(" ")
+                    dict = {items[0]: items[1]}
+                    for key, value in dict.items():
+                            if key == 'gender':
+                                if value == 'M':
+                                    self.count_gender_m += 1
+                                if value == 'F':
+                                    self.count_gender_f += 1
+                            elif key == 'age':
+                                self.age_list += [value]
+                            elif key == 'sales':
+                                if 0 <= int(value) >= 249:
+                                    self.count_sales_group1 += 1
+                                elif 250 <= int(value) >= 499:
+                                    self.count_sales_group2 += 1
+                                elif 500 <= int(value) >= 749:
+                                    self.count_sales_group3 += 1
+                                elif 750 <= int(value) >= 999:
+                                    self.count_sales_group4 += 1
+                            elif key == 'bmi':
+                                if value == 'Overweight':
+                                    self.count_bmi_ov += 1
+                                elif value == 'Obesity':
+                                    self.count_bmi_ob += 1
+                                elif value == 'Underweight':
+                                    self.count_bmi_un += 1
+                                elif value == 'Normal':
+                                    self.count_bmi_no += 1
+                            elif key == 'salary':
+                                self.salary_list += [value]
+                            elif key == 'birthday':
+                                month = value.split('/')[0]
+                                if month == '01':
+                                    self.count_birth_jan += 1
+                                elif month == '02':
+                                    self.count_birth_feb += 1
+                                elif month == '03':
+                                    self.count_birth_mar += 1
+                                elif month == '04':
+                                    self.count_birth_apr += 1
+                                elif month == '05':
+                                    self.count_birth_may += 1
+                                elif month == '06':
+                                    self.count_birth_jun += 1
+                                elif month == '07':
+                                    self.count_birth_jul += 1
+                                elif month == '08':
+                                    self.count_birth_aug += 1
+                                elif month == '09':
+                                    self.count_birth_sep += 1
+                                elif month == '10':
+                                    self.count_birth_oct += 1
+                                elif month == '11':
+                                    self.count_birth_nov += 1
+                                elif month == '12':
+                                    self.count_birth_dec += 1
 
     def line_chart(self):
         i = ChartLine()
