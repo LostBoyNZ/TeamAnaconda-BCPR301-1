45d47bd74f8f2402a875c650e65c383f40a784cf
Granity <30912719+Granity@users.noreply.github.com>
2018-03-13 21:18:06 +1300

    Add files via upload

--- file_reader.py
+++ file_reader.py
@@ -6,8 +6,6 @@ import os.path
 
 class FileReader(object):  # Claye
 
-    dict_root = {}
-
     def call_file(self, switch):
         y = input("Please enter the filename to read data from >>> ")
         try:
@@ -20,6 +18,7 @@ class FileReader(object):  # Claye
             self.call_file(switch)
 
     def split_file(self, file_name, switch): # Claye, Works with CSV and TXT docs
+        dict_root = {}
         file = open(file_name, "r")
         # Repeat for each line in the text file
         f = FileReader()
@@ -28,16 +27,16 @@ class FileReader(object):  # Claye
             # Split the file into different fields using "," to split fields
             fields = line.split(",")
             checked_id = DataProcessor.validate_key(fields[0])
-            if checked_id in f.dict_root:
+            if checked_id in dict_root:
                 dup_keys += 1
                 fields[6] = fields[6].rstrip()
                 data_to_log = "Duplicate Key" + str(fields[0:])
                 LogFileHandler.append_file('log.txt', data_to_log)
             else:
-                f.dict_root.update({checked_id: {'gender': fields[1], 'age': fields[2], 'sales': fields[3], 'bmi': fields[4], 'salary': fields[5], 'birthday': fields[6].rstrip(), 'valid': '0'}})
+                dict_root.update({checked_id: {'gender': fields[1], 'age': fields[2], 'sales': fields[3], 'bmi': fields[4], 'salary': fields[5], 'birthday': fields[6].rstrip(), 'valid': '0'}})
         # Close the file to free up resources (good practice)
         file.close()
-        valid_dict = DataProcessor.send_to_validate(f.dict_root, switch, dup_keys)
+        valid_dict = DataProcessor.send_to_validate(dict_root, switch, dup_keys)
         self.write_file(valid_dict)
 
     def write_file(self, dict_valid):
