b7bbadcb6b793029816dad72fdfa13d986a9c87c
Granity <30912719+Granity@users.noreply.github.com>
2018-03-13 17:41:07 +1300

    Add files via upload

--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,6 @@
 import errors
 from data_processor import DataProcessor
+from log_file_handler import LogFileHandler
 import os.path
 
 
@@ -7,31 +8,43 @@ class FileReader(object):  # Claye
 
     dict_root = {}
 
-    def call_file(self):
+    def call_file(self, switch):
         y = input("Please enter the filename to read data from >>> ")
         try:
-            self.split_file(y)
+            self.split_file(y, switch)
         except FileNotFoundError:
             print(errors.ErrorHandler.get_error_message(201))
+            self.call_file(switch)
+        except OSError:
+            print(errors.ErrorHandler.get_error_message(103))
+            self.call_file(switch)
 
-    def split_file(self, file_name): # Claye
+    def split_file(self, file_name, switch): # Claye, Works with CSV and TXT docs
         file = open(file_name, "r")
         # Repeat for each line in the text file
         f = FileReader()
+        dup_keys = 0
         for line in file:
             # Split the file into different fields using "," to split fields
             fields = line.split(",")
-            f.dict_root.update({fields[0]: {'gender': fields[1], 'age': fields[2], 'sales': fields[3], 'bmi': fields[4], 'salary': fields[5], 'birthday': fields[6].rstrip(), 'valid': '0'}})
+            checked_id = DataProcessor.validate_key(fields[0])
+            if checked_id in f.dict_root:
+                dup_keys += 1
+                fields[6] = fields[6].rstrip()
+                data_to_log = "Duplicate Key" + str(fields[0:])
+                LogFileHandler.append_file('log.txt', data_to_log)
+            else:
+                f.dict_root.update({checked_id: {'gender': fields[1], 'age': fields[2], 'sales': fields[3], 'bmi': fields[4], 'salary': fields[5], 'birthday': fields[6].rstrip(), 'valid': '0'}})
         # Close the file to free up resources (good practice)
         file.close()
-        valid_dict = DataProcessor.send_to_validate(f.dict_root)
+        valid_dict = DataProcessor.send_to_validate(f.dict_root, switch, dup_keys)
         self.write_file(valid_dict)
 
     def write_file(self, dict_valid):
         u = input("Are you sure you want to save data? Y/N >>> ")
         if u.upper() == "Y":
             file_target = input("Please input the filename to save to >>> ")
-            if os.path.exists("{}".format(file_target)):
+            if self.check_path_exists(file_target):
                 u2 = input("File exists, do you want to append the data Y/N >>> ")
                 if u2.upper() == 'Y':
                     self.commit_save(dict_valid, file_target)
@@ -46,6 +59,7 @@ class FileReader(object):  # Claye
             self.write_file(dict_valid)
 
     def commit_save(self, dict_valid, file_target):
+        try:
             z = open(file_target, "a")
             for key in dict_valid:
                 z.write("\n")
@@ -56,6 +70,22 @@ class FileReader(object):  # Claye
             z.write("\n")
             z.close()
             print("File saved")
+        except OSError:
+            print(errors.ErrorHandler.get_error_message(103))
+            self.write_file(dict_valid)
+
+
+    @staticmethod
+    def check_path_exists(path):
+        result = False
+        try:
+            if os.path.exists("{}".format(path)):
+                result = True
+                return result
+            else:
+                return result
+        except OSError:
+            print(errors.ErrorHandler.get_error_message(103))
 
 # i = FileReader()
 # i.call_file()
