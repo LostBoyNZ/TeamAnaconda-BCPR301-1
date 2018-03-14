b9999ade7aa2e7139bbeafebcb33d87311acf8fd
rmw0251 <30913291+rmw0251@users.noreply.github.com>
2018-03-14 23:35:52 +1300

    Add files via upload

--- commands/cmd_view.py
+++ commands/cmd_view.py
@@ -30,11 +30,11 @@ class View(Command):
     /?	Help about the View command
 
     [datatype]
-    	Specifies the datatype to display the chart for
+    Specifies the datatype to display the chart for
     """
 
     # do a prompt
-    # translates switches into the method names, e.g. /q switch would run quit
+    # translates switches into the method names, e.g. /l switch would run line chart
     def get_switch(self, switch):
         return {
             'l': self._line,
@@ -44,29 +44,27 @@ class View(Command):
         }.get(switch, '')
 
     # Rochelle
-    def get_data(self):
+    def get_data(self, choice):
         # choose file to get data from
         file_name = input("Please enter the filename to read data from >>> ")
         # check file exists
         try:
             file_contents = self._load_file_data(file_name)
-
+            # create instance of CalcData to calc data for particular chart
             i = CalcData()
 
-            if file_contents:
-                if i.is_valid(file_contents):
-                    i.calculate(file_contents)
-                    return i
+            if file_contents:  # if contents isn't empty
+                if i.is_valid(file_contents):  # as long as contents are valid
+                    i.calculate(file_contents, choice)  # calculate the chart data
+                    return i  # return i so chart can be called
                 else:
                     print(err.get_error_message(210))
-
         except FileNotFoundError:
             print(errors.ErrorHandler.get_error_message(201))
 
     # Graham
     def _load_file_data(self, file_name):
         file_contents = []
-
         try:
             with open(file_name, "r") as file:
                 for line in file:
@@ -84,27 +82,34 @@ class View(Command):
 
     def _line(self):
         # line chart for sales and salary
-        valid_data = self.get_data()
-        if valid_data:
-            valid_data.line_chart()
+        if self.user_string:
+            print(err.get_error_message(102))
+        else:
+            valid_data = self.get_data('line')
+            if valid_data:
+                valid_data.line_chart()
 
     def _bar(self):
         # bar chart for birthday or bmi
         if self.user_string == 'bmi' or self.user_string == 'birthday':
-            valid_data = self.get_data()
+            valid_data = self.get_data(self.user_string)
             if valid_data:
-                valid_data.bar_chart(self.user_string)
+                valid_data.bar_chart(self.user_string)  # user string for checking which chart dat type
         else:
             print(err.get_error_message(102))
 
     def _pie(self):
         if self.user_string == 'sales' or self.user_string == 'gender':
-            valid_data = self.get_data()
+            valid_data = self.get_data(self.user_string)
             if valid_data:
-                pass
-                # valid_data.pie_chart(self.user_string)
+                valid_data.pie_chart(self.user_string)
         else:
             print(err.get_error_message(102))
 
     def _help(self):
         print(self.__doc__)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
