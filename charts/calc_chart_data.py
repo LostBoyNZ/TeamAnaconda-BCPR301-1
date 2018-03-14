f80cb8ffb17a4cfe582ede633287f0dc5be65fa7
rmw0251 <30913291+rmw0251@users.noreply.github.com>
2018-03-14 22:15:50 +1300

    Add files via upload

--- charts/calc_chart_data.py
+++ charts/calc_chart_data.py
@@ -1,7 +1,23 @@
+# Rochelle
 import sys
-from charts.chart_line import ChartLine
-from charts.chart_bar import ChartBar
-from charts.chart_pie import ChartPie
+
+try:
+    from charts.chart_line import ChartLine
+except NameError and ModuleNotFoundError and ImportError:
+    print("Fatal Error - chart_line.py not found.")
+    sys.exit()
+
+try:
+    from charts.chart_bar import ChartBar
+except NameError and ModuleNotFoundError and ImportError:
+    print("Fatal Error - chart_bar.py not found.")
+    sys.exit()
+
+try:
+    from charts.chart_pie import ChartPie
+except NameError and ModuleNotFoundError and ImportError:
+    print("Fatal Error - chart_pie.py not found.")
+    sys.exit()
 
 try:
     from errors import ErrorHandler as err
@@ -13,6 +29,7 @@ except NameError and ModuleNotFoundError and ImportError:
 class CalcData(object):
 
     def __init__(self):
+        # create counts for chart data
         self.count_gender_m = 0
         self.count_gender_f = 0
         self.count_bmi_ov = 0
@@ -38,86 +55,126 @@ class CalcData(object):
         self.age_list = []
         self.salary_list = []
 
+    # Rochelle
+    # checks data inside file is correct to make a chart
     def is_valid(self, file_contents):
-        result = False
-        values = ['gender', 'age', 'birthday', 'bmi', 'sales', 'salary']
-        contents = ''.join(file_contents)
-        count = 0
-        for value in values:
-            if value in contents:
-                count += 1
-        if count == len(values):
-            result = True
-        return result
+        """
+        >>> i = CalcData()
+        >>> i.is_valid("A001,gender M,age 52,sales 123,bmi Overweight,salary 50,birthday 23/10/1998,valid 1,")
+        True
 
-    def calculate(self, file_contents):
-        dict_data = {}
+        >>> i.is_valid("a001,M,52,123,Overweight,50,23-10-1998 Z005,F,18,624,Normal,85,25-04-1158 C078,F,35")
+        False
+
+        >>> i.is_valid("class ErrorHandler(object):  # Claye @staticmethod def get_error_message(error_code, ")
+        False
+
+        :param file_contents:
+        :return:
+        """
+        result = False
+        try:
+            values = ['gender', 'age', 'birthday', 'bmi', 'sales', 'salary']
+            contents = ''.join(file_contents)
+            count = 0
+            for value in values:
+                if value in contents:
+                    count += 1
+            if count == len(values):
+                result = True
+            return result
+        except TypeError:
+            return result
+
+    def calc_line(self, key, value):
+        if key == 'age':
+            self.age_list += [value]
+        elif key == 'salary':
+            self.salary_list += [value]
+
+    def calc_pie_gender(self, value):
+        if value == 'M':
+            self.count_gender_m += 1
+        if value == 'F':
+            self.count_gender_f += 1
+
+    def calc_pie_sales(self, value):
+        sales = int(value)
+        try:
+            if 0 <= sales <= 249:
+                self.count_sales_group1 += 1
+            elif 250 <= sales <= 499:
+                self.count_sales_group2 += 1
+            elif 500 <= sales <= 749:
+                self.count_sales_group3 += 1
+            elif 750 <= sales <= 999:
+                self.count_sales_group4 += 1
+        except ValueError:
+            print("invalid integer")
+
+    def calc_bar_bmi(self, value):
+        if value == 'Overweight':
+            self.count_bmi_ov += 1
+        elif value == 'Obesity':
+            self.count_bmi_ob += 1
+        elif value == 'Underweight':
+            self.count_bmi_un += 1
+        elif value == 'Normal':
+            self.count_bmi_no += 1
+
+    # calculates number of people born in a month
+    def calc_bar_birthday(self, value):
+        month = value.split('/')[1]
+        if month == '01':
+            self.count_birth_jan += 1
+        elif month == '02':
+            self.count_birth_feb += 1
+        elif month == '03':
+            self.count_birth_mar += 1
+        elif month == '04':
+            self.count_birth_apr += 1
+        elif month == '05':
+            self.count_birth_may += 1
+        elif month == '06':
+            self.count_birth_jun += 1
+        elif month == '07':
+            self.count_birth_jul += 1
+        elif month == '08':
+            self.count_birth_aug += 1
+        elif month == '09':
+            self.count_birth_sep += 1
+        elif month == '10':
+            self.count_birth_oct += 1
+        elif month == '11':
+            self.count_birth_nov += 1
+        elif month == '12':
+            self.count_birth_dec += 1
+
+    # if person has valid data - send to appropriate function depending on their chart choice
+    def calculate(self, file_contents, choice):
         for line in file_contents[1:]:
             fields = line.split(",")
             if 'valid 1' in fields:
                 for item in fields[1:-1]:
                     items = item.split(" ")
-                    dict = {items[0]: items[1]}
-                    for key, value in dict.items():
-                            if key == 'gender':
-                                if value == 'M':
-                                    self.count_gender_m += 1
-                                if value == 'F':
-                                    self.count_gender_f += 1
-                            elif key == 'age':
-                                self.age_list += [value]
-                            elif key == 'sales':
-                                sales = int(value)
-                                if 0 <= sales <= 249:
-                                    self.count_sales_group1 += 1
-                                elif 250 <= sales <= 499:
-                                    self.count_sales_group2 += 1
-                                elif 500 <= sales <= 749:
-                                    self.count_sales_group3 += 1
-                                elif 750 <= sales <= 999:
-                                    self.count_sales_group4 += 1
-                            elif key == 'bmi':
-                                if value == 'Overweight':
-                                    self.count_bmi_ov += 1
-                                elif value == 'Obesity':
-                                    self.count_bmi_ob += 1
-                                elif value == 'Underweight':
-                                    self.count_bmi_un += 1
-                                elif value == 'Normal':
-                                    self.count_bmi_no += 1
-                            elif key == 'salary':
-                                self.salary_list += [value]
-                            elif key == 'birthday':
-                                month = value.split('/')[0]
-                                if month == '01':
-                                    self.count_birth_jan += 1
-                                elif month == '02':
-                                    self.count_birth_feb += 1
-                                elif month == '03':
-                                    self.count_birth_mar += 1
-                                elif month == '04':
-                                    self.count_birth_apr += 1
-                                elif month == '05':
-                                    self.count_birth_may += 1
-                                elif month == '06':
-                                    self.count_birth_jun += 1
-                                elif month == '07':
-                                    self.count_birth_jul += 1
-                                elif month == '08':
-                                    self.count_birth_aug += 1
-                                elif month == '09':
-                                    self.count_birth_sep += 1
-                                elif month == '10':
-                                    self.count_birth_oct += 1
-                                elif month == '11':
-                                    self.count_birth_nov += 1
-                                elif month == '12':
-                                    self.count_birth_dec += 1
+                    dict_data = {items[0]: items[1]}
+                    for key, value in dict_data.items():
+                        if choice == 'line':
+                            self.calc_line(key, value)
+                        elif key == 'gender' and choice == 'gender':
+                            self.calc_pie_gender(value)
+                        elif key == 'sales' and choice == 'sales':
+                            self.calc_pie_sales(value)
+                        elif key == 'bmi' and choice == 'bmi':
+                            self.calc_bar_bmi(value)
+                        elif key == 'birthday' and choice == 'birthday':
+                            self.calc_bar_birthday(value)
 
     def line_chart(self):
         i = ChartLine()
         i.create_line_grid(self.age_list, self.salary_list)
 
+    # depending on user chart choice add data to chart to output
     def bar_chart(self, choice):
         i = ChartBar()
         if choice == 'bmi':
@@ -125,14 +182,17 @@ class CalcData(object):
             ylabel = 'Number'
             objects = ('Normal', 'Overweight', 'Obesity', 'Underweight')
             data = [self.count_bmi_ov, self.count_bmi_ob, self.count_bmi_un, self.count_bmi_no]
-            i.create_bar_chart(title, ylabel, objects, data)
+            fig_title = 'BMI Chart'
+            i.create_bar_chart(title, ylabel, objects, data, fig_title)
         elif choice == 'birthday':
             title = 'Birth Months'
             ylabel = 'Number'
             objects = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
-            data = [self.count_birth_jan, self.count_birth_feb, self.count_birth_mar, self.count_birth_apr, self.count_birth_may, self.count_birth_jun,
-                    self.count_birth_jul, self.count_birth_aug, self.count_birth_sep, self.count_birth_oct, self.count_birth_nov, self.count_birth_dec]
-            i.create_bar_chart(title, ylabel, objects, data)
+            data = [self.count_birth_jan, self.count_birth_feb, self.count_birth_mar, self.count_birth_apr,
+                    self.count_birth_may, self.count_birth_jun, self.count_birth_jul, self.count_birth_aug,
+                    self.count_birth_sep, self.count_birth_oct, self.count_birth_nov, self.count_birth_dec]
+            fig_title = 'Birth Months Chart'
+            i.create_bar_chart(title, ylabel, objects, data, fig_title)
 
     def pie_chart(self, choice):
         i = ChartPie()
@@ -142,3 +202,8 @@ class CalcData(object):
         elif choice == 'gender':
             data = [self.count_gender_f, self.count_gender_m]
             i.create_pie_chart(data, choice)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
